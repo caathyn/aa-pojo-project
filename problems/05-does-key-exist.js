cc01e638c2fff0a827d2a27dbb9db21f49d9ca05
Cathy Nguyen <102982962+caathyn@users.noreply.github.com>
2023-08-16 16:33:33 -0700

    Add files via upload

---
+++ problems/05-does-key-exist.js
@@ -0,0 +1,25 @@
+/***********************************************************************
+Write a function `doesKeyExist(obj, key)` that takes in an object and a
+key and returns true if the key is inside the object and false if the
+key is not inside the object.
+
+Examples:
+let obj1 = {bootcamp: 'App Academy', course: 'Bootcamp Prep'}
+
+console.log(doesKeyExist(obj1, 'course')); // => true
+console.log(doesKeyExist(obj1, 'bootcamp')); // => true
+console.log(doesKeyExist(obj1, 'name')); // => false
+console.log(doesKeyExist(obj1, 'established')); // => false
+
+***********************************************************************/
+
+function doesKeyExist(obj, key) {
+  if (obj[key] === undefined) {
+    return false;
+  }
+  
+  return true;
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = doesKeyExist;
